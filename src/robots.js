e6038842f428f2b198e6d441752478d7e64c846a
dabogdan <39666410+dabogdan@users.noreply.github.com>
2020-06-04 20:26:21 +0300

    initial upload of the project

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 'dashakakasha',
+    name: 'Dasha Kakasha',
+    username: 'Dasha',
+    email: 'dasha@kakasha.biz'
+  },
+  {
+    id: 'musiakukusia',
+    name: 'Musia Kukusia',
+    username: 'Musia',
+    email: 'Musia@kukusia.tv'
+  },
+  {
+    id: 'bagdaniaskisus',
+    name: 'Bagdania Skisus',
+    username: 'Dimon',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
